4607413d9652d0095e516615d0d751e4e49a5c8d
floteddy <86662600+floteddy@users.noreply.github.com>
2021-07-08 10:15:02 +0300

    Add files via upload

---
+++ var.py
@@ -0,0 +1,18 @@
+We can use matching double quotes for string literals
+We can use comparison operators to compare strings
+The expression 1/0 evaluates an error
+"=" is for identity and "=="is for equality
+a=b=c=17
+a= 1
+a= 1+1
+a= 2+2
+a= 4+4
+a= True
+a= not a
+a= not a
+a= not a
+a= 2
+a= 2 * 2
+a= 4 * 4
+a= 16 * 16
+a= 256
